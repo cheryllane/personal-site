b727d0c5a4c403fa4b6ba876dc13ff5786a6caf2
Cheryl <cherylane@live.co.uk>
2025-04-20 21:06:27 +0100

    add page title support to router

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import AboutMe from "./pages/AboutMe";
 import Home from "./pages/Home";
 import Stardew from "./pages/Stardew";
 
+const Page = ({ child, title }: { child: JSX.Element; title?: string }) => {
+    // Allow an optional title prop to be passed that sets the page title
+    if (title) {
+        document.title = title;
+    }
+    return child;
+};
+
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<AboutMe />} />
-                <Route path="/stardew" element={<Stardew />} />
+                <Route path="/" element={<Page child={<Home />} />} />
+                <Route
+                    path="/about"
+                    element={<Page child={<AboutMe />} title="About Me" />}
+                />
+                <Route
+                    path="/stardew"
+                    element={<Page child={<Stardew />} title="Stardew" />}
+                />
             </Routes>
         </BrowserRouter>
     );
